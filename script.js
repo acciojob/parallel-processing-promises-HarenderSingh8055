a835e86934331824d80728e7b123858d938ad089
HarenderSingh8055 <124910145+HarenderSingh8055@users.noreply.github.com>
2023-04-30 18:44:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 //your JS code here. If required.
-const output = document.getElementById("output");
-const btn = document.getElementById("download-images-button");
-
 const images = [
-   {
+  {
     url: "https://picsum.photos/id/237/200/300",
     alt: "Image 1",
   },
@@ -15,34 +12,38 @@ const images = [
     url: "https://picsum.photos/id/239/200/300",
     alt: "Image 3",
   }
+  
 ];
-btn.addEventListener("click",downloadImages(images));
 
+function downloadImages(images) {
+  const promises = images.map(image => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.src = image.url;
+      img.alt = image.alt;
+      img.onload = () => {
+        resolve(img);
+      };
+      img.onerror = () => {
+        reject(`Failed to load image's URL: ${image.url}`);
+      };
+    });
+  });
 
-
-function downloadImages(images){
-	const allPromises = images.map((image)=>{
-		return new Promise((resolve,reject)=>{
-            const img = new Image();
-            img.src = `${image.url}`;
-            img.alt = `${image.alt}`;
-        
-            img.onload = ()=>{
-                resolve(img);
-            }
-        
-            img.onerror = ()=>{
-                reject(`Failed to load image's URL: ${image.url}`);
-            };
-        });;
-	});
-
-	Promise.all(allPromises).then((imgUrl)=>{
+  Promise.all(promises)
+    .then(imgs => {
+      const output = document.getElementById('output');
 		output.innerHTML = null;
-		imgUrl.forEach((img)=>{
-			output.appendChild(img);
-		})
-	}).catch((error)=>{
-		console.error(error);
-	});
-};
+      imgs.forEach(img => {
+        output.appendChild(img);
+      });
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+const button = document.getElementById('download-images-button');
+button.addEventListener('click', () => {
+  downloadImages(images);
+});
